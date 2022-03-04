05fb00369018eabeb8bd8c96967f90c1c098ab73
Subhashis Hansda <subhashishansda2@gmail.com>
2022-03-04 16:59:12 +0530

    notification for drinking water every half an hour

--- notification.py
+++ notification.py
@@ -5,3 +5,19 @@ Created on Fri Mar  4 16:53:05 2022
 @author: VAGUE
 """
 
+from plyer import notification
+import time
+
+def drink_water():
+    title = "Time to Get Hydrated !!"
+    message = "Get up and drink a glass of water"
+    while True:
+        notification.notify(
+            title = title,
+            message = message,
+            timeout = 5
+        )
+        time.sleep(1800)
+
+if __name__ == "__main__":
+    drink_water()
